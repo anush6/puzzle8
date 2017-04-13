939b52cb794954759e56d0f6636d02ef3adc3d58
anush6 <anush070@gmail.com>
2017-04-13 21:57:34 +0530

    statement

--- app/src/main/java/com/google/engedu/puzzle8/PuzzleActivity.java
+++ app/src/main/java/com/google/engedu/puzzle8/PuzzleActivity.java
@@ -9,7 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
-
+//new commit
 
 public class PuzzleActivity extends AppCompatActivity {
 
